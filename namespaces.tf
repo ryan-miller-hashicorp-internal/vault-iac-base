ccca781f24cc15302c0205980d2e9e91bd0f2617
Ryan Miller <RyanMillerC@outlook.com>
2025-11-05 12:39:44 -0500

    Remove -test suffix from namespace

--- namespaces.tf
+++ namespaces.tf
@@ -15,5 +15,5 @@ module "vault-namespace" {
   source  = "app.terraform.io/ryan-miller-hashicorp-internal/vault-namespace/ryanmillerc"
   version = "0.0.1"
 
-  name = "aa0001-test"
+  name = "aa0001"
 }
