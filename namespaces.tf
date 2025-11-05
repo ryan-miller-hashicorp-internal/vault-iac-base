d2aa542caeabc5468170d07bf487ea3c37d6fa9d
Ryan Miller <RyanMillerC@outlook.com>
2025-11-05 12:14:55 -0500

    Create tenant namespace using module

--- namespaces.tf
+++ namespaces.tf
@@ -1,3 +1,19 @@
+#=========================
+# Create parent namespace
+#=========================
+
 resource "vault_namespace" "this" {
   path = "tenant"
+}
+
+
+#==========================
+# Create tenant namespaces
+#==========================
+
+module "vault-namespace" {
+  source  = "app.terraform.io/ryan-miller-hashicorp-internal/vault-namespace/ryanmillerc"
+  version = "0.0.1"
+
+  name = "aa0001-test"
 }
