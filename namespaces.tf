384c7fec8bfc7055eb8a19d171e02f24af23c932
Ryan Miller <RyanMillerC@outlook.com>
2025-11-05 13:06:54 -0500

    Create aa0002 namespace

--- namespaces.tf
+++ namespaces.tf
@@ -17,3 +17,10 @@ module "vault-namespace" {
 
   name = "aa0001"
 }
+
+module "vault-namespace" {
+  source  = "app.terraform.io/ryan-miller-hashicorp-internal/vault-namespace/ryanmillerc"
+  version = "0.0.2"
+
+  name = "aa0002"
+}
